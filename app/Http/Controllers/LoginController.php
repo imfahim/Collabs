9945ca254e7ec95166f12bda5490d3edc3e48d5c
LAPTOP-Q8FDA8A6\Jihan <rafat0819@gmail.com>
2017-11-02 16:02:15 +0600

    New Project.

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests\LoginRequest;
+use Session;
+use Illuminate\Support\Facades\DB;
+
+
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('login');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function verify(LoginRequest $request)
+    {
+        $user = DB::table('users')
+          ->where('email', $request->Email)
+          ->where('password', $request->Pass)
+          ->first();
+
+        if($user){
+
+          if($user->type == 1){
+            Session::flash('success', 'Successfully Logged In!' );
+            Session::push('id', $user->id);
+
+            return redirect()->route('user.index');
+          }
+
+
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/login.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<table border="0" width="100%">
+		<tr>
+			<td width="100"></td>
+			<td align="center">
+				<h1>User</h1>
+			</td>
+			<td width="100"></td>
+		</tr>
+		<tr>
+			<td width="100"></td>
+			<td align="center">
+				<hr/>
+			</td>
+			<td width="100"></td>
+		</tr>
+		<tr>
+			<td width="100"></td>
+			<td>
+				<br/>
+				<center>
+				<h3>Login</h3>
+					<form method="post" action="{{route('login.verify')}}">
+						{{ csrf_field() }}
+						<table>
+							<tr>
+								<td>Email: </td>
+								<td><input type="text" name="Email"></td>
+							</tr>
+							<tr>
+								<td>PASSWORD: </td>
+								<td><input type="password" name="Pass"></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td><input type="submit" value="Login"></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td>
+									<br/>
+									Click <a href="">here</a> to register
+								</td>
+							</tr>
+						</table>
+					</form>
+					<br/>
+					<br/>
+					<div>
+						@if(Session::has('success'))
+							<p style="color: green;">{{ Session::get('success') }}</p>
+						@endif
+						@if(Session::has('fail'))
+							<p style="color: red;">{{ Session::get('fail') }}</p>
+						@endif
+					</div>
+				</center>
+			</td>
+			<td width="100"></td>
+		</tr>
+	</table>
+</body>
+</html>
