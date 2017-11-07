3642c729c697159edc6604515b430c69cf14007d
LAPTOP-Q8FDA8A6\Jihan <rafat0819@gmail.com>
2017-11-07 18:56:48 +0600

    Registration Done

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -20,13 +20,13 @@ class RegisterController extends Controller
     }
     public function store(RegisterRequest $request)
     {
-
+        $created_at = date('Y/m/d h:i:s', time());
         $id =DB::table('users')->insertGetId(
-                ['name' => $request->input('name'),'email' => $request->input('email'), 'password' => $request->input('password'),'type'=>$request->input('type')]
+                ['name' => $request->input('name'),'email' => $request->input('email'), 'password' => $request->input('password'),'type'=>$request->input('type'),'created_at' => $created_at ]
         );
 
 
-        $request->session()->put('name', $request->input('username'));
+        $request->session()->put('name', $request->input('name'));
         $request->session()->put('id', $id);
 
         if($request->input('type')==0){
